1d297d5f2d173d8a077a725d9764dcaca05507c7
fateme <fatemehbaneshi2016@gmail.com>
2024-04-08 13:15:19 +0330

    next edite

---
+++ src/utils/auth.js
@@ -0,0 +1,44 @@
+import { hash, compare } from "bcryptjs";
+import { sign, verify } from "jsonwebtoken";
+
+const hashPassword = async (password) => {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+};
+
+const generateAccessToken = (data) => {
+  const token = sign({ ...data }, process.env.AccessTokenSecretKey, {
+    expiresIn: "60s",
+  });
+  return token;
+};
+
+const verifyAccessToken = (token) => {
+  try {
+    const tokenPayload = verify(token, process.env.AccessTokenSecretKey);
+    return tokenPayload;
+  } catch (err) {
+    console.log("Verify Access Token Error ->", err);
+    return false;
+  }
+};
+
+const generateRefreshToken = (data) => {
+  const token = sign({ ...data }, process.env.RefreshTokenSecretKey, {
+    expiresIn: "15d",
+  });
+  return token;
+};
+
+export {
+  hashPassword,
+  verifyPassword,
+  generateAccessToken,
+  verifyAccessToken,
+  generateRefreshToken,
+};
