e61e3b28801969d0273ab5c2124a9df434b17439
fateme <fatemehbaneshi2016@gmail.com>
2024-04-13 10:31:22 +0330

    18 edits

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -35,10 +35,29 @@ const generateRefreshToken = (data) => {
   return token;
 };
 
+const valiadteEmail = (email) => {
+  const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
+  return pattern.test(email);
+};
+
+const valiadtePhone = (phone) => {
+  const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+  return pattern.test(phone);
+};
+
+const valiadtePassword = (password) => {
+  const pattern =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  return pattern.test(password);
+};
+
 export {
   hashPassword,
   verifyPassword,
   generateAccessToken,
   verifyAccessToken,
   generateRefreshToken,
+  valiadteEmail,
+  valiadtePhone,
+  valiadtePassword,
 };
