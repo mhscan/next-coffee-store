e3464771142ffafb0305abd58f2f94cf471d7794
mhscan <mr.rezaeeian@gmail.com>
2024-04-14 14:01:20 +0330

    nav bar links logic

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,7 +13,7 @@ const verifyPassword = async (password, hashedPassword) => {
 
 const generateAccessToken = (data) => {
   const token = sign({ ...data }, process.env.AccessTokenSecretKey, {
-    expiresIn: "60s",
+    expiresIn: "60d",
   });
   return token;
 };
