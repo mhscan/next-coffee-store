51c581270e4b72b304e7922463c507cc17aeeced
fateme <fatemehbaneshi2016@gmail.com>
2024-05-01 18:26:54 +0330

    add to whish list in client

--- src/components/templates/product/AddToWishlist.js
+++ src/components/templates/product/AddToWishlist.js
@@ -1,10 +1,53 @@
 "use client";
-import React from "react";
+import { showSwal } from "@/utils/helpers";
+import React, { useEffect, useState } from "react";
 import { CiHeart } from "react-icons/ci";
 
-async function AddToWishlist() {
-  const addToWishlist = async () => {
-    console.log("add to Wishlist event handler");
+function AddToWishlist({ productID }) {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const authUser = async () => {
+      const res = await fetch("/api/auth/me");
+      console.log(res);
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log(data);
+        setUser({ ...data });
+      }
+    };
+
+    authUser();
+  }, []);
+
+  const addToWishlist = async (event) => {
+    event.preventDefault();
+    if (!user?._id) {
+      return showSwal(
+        "برای اضافه کردن به علاقه مندی‌ها لطفا ابتدا لاگین بکنین",
+        "error",
+        "فهمیدم"
+      );
+    }
+
+    const wish = {
+      user: user._id,
+      product: productID,
+    };
+
+    const res = await fetch("/api/wishlist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(wish),
+    });
+
+    console.log("Response ->", res);
+
+    if (res.status === 201) {
+      showSwal("محصول مورد نظر به علاقه‌مندی‌ها اضافه شد", "success", "فهمیدم");
+    }
   };
 
   return (
