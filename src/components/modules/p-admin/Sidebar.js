9700710cbf240b797e84f683924ccbcb5d2a098d
mhscan <mr.rezaeeian@gmail.com>
2024-05-27 13:41:32 +0330

    p-admin-logout

--- src/components/modules/p-admin/Sidebar.js
+++ src/components/modules/p-admin/Sidebar.js
@@ -4,21 +4,36 @@ import { ImReply } from "react-icons/im";
 import { FaComments, FaHeart, FaShoppingBag, FaUsers } from "react-icons/fa";
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { MdSms, MdLogout } from "react-icons/md";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { TbListDetails } from "react-icons/tb";
 import Link from "next/link";
 import swal from "sweetalert";
 
 const Sidebar = () => {
   const path = usePathname();
+  const router = useRouter();
 
   const logoutHandler = () => {
     swal({
       title: "آیا از خروج اطمینان دارید؟",
       icon: "warning",
       buttons: ["نه", "آره"],
-    }).then((result) => {
-      //code
+    }).then(async (result) => {
+      if (result) {
+        const res = await fetch("/api/auth/signout", {
+          method: "POST",
+        });
+
+        if (res.status === 200) {
+          swal({
+            title: "با موفقیت از اکانت خارج شدین",
+            icon: "success",
+            buttons: "فهمیدم",
+          }).then((result) => {
+            router.replace("/");
+          });
+        }
+      }
     });
   };
   return (
