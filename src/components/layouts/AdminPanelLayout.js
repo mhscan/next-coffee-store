397bdfb2aa089d17b174fbbfdf1eb0c57e751adc
mhscan <mr.rezaeeian@gmail.com>
2024-05-27 13:41:00 +0330

    p-admin-auth

--- src/components/layouts/AdminPanelLayout.js
+++ src/components/layouts/AdminPanelLayout.js
@@ -2,8 +2,19 @@ import React from "react";
 import styles from "./adminPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-admin/Sidebar";
 import Topbar from "@/components/modules/p-admin/Topbor";
+import { redirect } from "next/navigation";
+import { authUser } from "@/utils/serverHelpers";
 
-const Layout = ({ children }) => {
+const Layout = async ({ children }) => {
+  const user = await authUser();
+  if (user) {
+    if (user.role !== "ADMIN") {
+      return redirect("/login-register");
+    }
+  } else {
+    return redirect("/login-register");
+  }
+
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
